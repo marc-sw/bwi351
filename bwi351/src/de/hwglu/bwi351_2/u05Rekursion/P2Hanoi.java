1e4b7c360cea38f62df80d52e92a3f5459933432
marc <marc.decker.public@gmail.com>
2024-11-09 15:55:24 +0100

    hanoi impl

--- bwi351/src/de/hwglu/bwi351_2/u05Rekursion/P2Hanoi.java
+++ bwi351/src/de/hwglu/bwi351_2/u05Rekursion/P2Hanoi.java
@@ -1,16 +1,16 @@
 package de.hwglu.bwi351_2.u05Rekursion;
 
 public class P2Hanoi {
+
   static void bewege(int von, int nach) {
    System.out.println("Eine Scheibe von Stapel " + von + " nach Stapel " + nach);
   }
+
   static void hanoi(int groesse, int von, int nach, int arbber) {
-// hier kommt der Algorithmus rein
-
-
-//  so wird die Bewegung einer einzelnen Scheibe abgebildet
-        bewege(von, nach);
-
+      if (groesse < 1) return;
+      hanoi(--groesse, von, arbber, nach);
+      bewege(von, nach);
+      hanoi(groesse, arbber, nach, von);
   }
 
   public static void main(String[] args) {
